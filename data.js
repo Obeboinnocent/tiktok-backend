438fb517892d06546143466ef351c3e15d63f734
Obeboinnocent <obeboinnocent@gmail.com>
2021-03-21 15:34:36 +0300

    First time

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    url: "vid1.mp4",
+    channel: "ObeboInnocent",
+    description: "Epic Tiktok Build",
+    song: "Saturday Night by Khaled",
+    likes: "406",
+    messages: "79",
+    shares: "762",
+  },
+  {
+    url: "vid2.mp4",
+    channel: "Baraka Obebo",
+    description: "EXploring and adventuring in my area",
+    song: "Prayed for you bt Matt Stell",
+    likes: "6",
+    messages: "759",
+    shares: "362",
+  },
+];
+
+export default data;
